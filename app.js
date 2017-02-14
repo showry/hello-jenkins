3aef2ff92c190848c5d942333525b1ad6a844f67
showry <showry28@gmail.com>
2017-02-14 19:43:33 +0530

    updated app.js
    sample test1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello sample devops demo1 ');
+  res.send('hello sample devops demo1');
   
 });
  
